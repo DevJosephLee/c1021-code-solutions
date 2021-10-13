6904ba9d977ae8fffc7b8a2d8fed483722d88b80
Joseph Lee <teddylou0215@gmail.com>
2021-10-12 23:24:00 -0700

    fixed items property to array

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -4,39 +4,39 @@ var orderHistory = [
     orderPlaced: 'Aug 4, 2020',
     delivered: 'Aug 8, 2020',
     shipTo: 'JS Masher',
-    items: {
+    items: [{
       title: 'JavaScript for Impatient programmers',
       author: 'Rauschmayer, Dr. Axel',
       returnWindowClosed: 'Sep 7, 2020',
-      price: '$31.55'
-    },
-    total: '$34.00'
+      price: 31.55
+    }],
+    total: 34.00
   },
   {
     orderNumber: '112 - 9984268 - 1280257',
     orderPlaced: 'Jul 19, 2020',
     delivered: 'Jul 20, 2020',
     shipTo: 'JS Masher',
-    items: {
+    items: [{
       title: 'The Timeless Way of Building',
       author: 'Alexander, Christopher',
       returnWindowClosed: 'Aug 19, 2020',
-      price: '$41.33'
-    },
-    total: '$44.53'
+      price: 41.33
+    }],
+    total: 44.53
   },
   {
     orderNumber: '114 - 2875557 - 9059409',
     orderPlaced: 'Jul 4, 2020',
     delivered: 'Jul 7, 2020',
     shipTo: 'JS Masher',
-    items: {
+    items: [{
       title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U...',
       author: null,
       returnWindowClosed: 'Aug 5, 2020',
-      price: '$15.98'
-    },
-    total: '$17.22'
+      price: 15.98
+    }],
+    total: 17.22
   },
   {
     orderNumber: '113 - 2883177 - 2648248',
@@ -47,15 +47,15 @@ var orderHistory = [
       title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
       author: null,
       returnWindowClosed: 'Aug 4, 2020',
-      price: '$94.95'
+      price: 94.95
     },
     {
       title: 'The Art of Sql',
       author: 'Faroult, Stephane',
       returnWindowClosed: 'Aug 4, 2020',
-      price: '$33.99'
+      price: 33.99
     }],
-    total: '$138.93'
+    total: 138.93
   }
 ];
 
