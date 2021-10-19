b43dc84f855052b2f638386b12ae35f5f8263226
Joseph Lee <teddylou0215@gmail.com>
2021-10-19 15:44:10 -0700

    removed unnecessary code

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,17 +1,14 @@
 var $modalButton = document.querySelector('.open-modal-button');
 var $noButton = document.querySelector('.no-button');
-var $background = document.querySelector('.bg-white');
 var $hidden = document.querySelector('.hidden');
 
 $modalButton.addEventListener('click', clickModalButton);
 $noButton.addEventListener('click', clickNoButton);
 
 function clickModalButton(event) {
-  $background.className = 'bg-323232';
   $hidden.className = 'show';
 }
 
 function clickNoButton(event) {
-  $background.className = 'bg-white';
   $hidden.className = 'hidden';
 }
