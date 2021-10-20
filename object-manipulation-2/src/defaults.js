d291a0d739cdbee8c833b75f59a330e255984279
Joseph Lee <teddylou0215@gmail.com>
2021-10-20 14:31:41 -0700

    in progress

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,4 +1,4 @@
 /* exported defaults */
 function defaults(target, source) {
-
+  Object.assign(target, source);
 }
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,15 +1,32 @@
 /* exported omit */
-function omit(source, keys) {
-  var output = {};
-  var entries = Object.entries(source);
-  for (var i = 0; i < entries.length; i++) {
-    var key = entries[i][0];
-    var values = entries[i][1];
-    for (var j = 0; j < keys.length; j++) {
-      if (key !== keys[j]) {
-        output[key] = values;
-      }
-    }
-  }
-  return output;
-}
+// function omit(source, keys) {
+//   var output = {};
+//   var entries = Object.entries(source);
+//   var key = [];
+//   var values = [];
+//   for (var i = 0; i < entries.length; i++) {
+//     key = entries[i][0];
+//     values = entries[i][1];
+//     for (var j = 0; j < keys.length; j++) {
+//       if (!keys[j].includes(key)) {
+//         console.log(key);
+//       }
+//     }
+//   }
+//   return output;
+// }
+
+// function omit(source, keys) {
+//   var output = {};
+//   var entries = Object.entries(source);
+//   for (var i = 0; i < entries.length; i++) {
+//     var key = entries[i][0];
+//     var values = entries[i][1];
+//     for (var j = 0; j < keys.length; j++) {
+//       if (key !== keys[j]) {
+//         output[key] = source[values];
+//       }
+//     }
+//   }
+//   return output;
+// }
