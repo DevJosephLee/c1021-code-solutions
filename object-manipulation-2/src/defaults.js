45717319d5f59595473c31543b56d57076edd20d
Joseph Lee <teddylou0215@gmail.com>
2021-10-20 22:36:15 -0700

    in progress

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,4 +1,16 @@
 /* exported defaults */
 function defaults(target, source) {
-  Object.assign(target, source);
+  // Object.assign(target, source);
+  // var entriesTarget = Object.entries(target);
+  // var entriesSource = Object.entries(source);
+  // console.log('target:', entriesTarget);
+  // console.log('source:', entriesSource);
+  // for (var i = 0; i < entriesTarget.length; i++) {
+  //   for (var j = 0; j < entriesSource.length; j++) {
+  //     if (entriesTarget[i][0] !== entriesSource[j][0]) {
+  //       entriesTarget.push(entriesSource[j]);
+  //     }
+  //   }
+  // }
+
 }
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -20,13 +20,13 @@
 //   var output = {};
 //   var entries = Object.entries(source);
 //   for (var i = 0; i < entries.length; i++) {
-//     var key = entries[i][0];
-//     var values = entries[i][1];
 //     for (var j = 0; j < keys.length; j++) {
-//       if (key !== keys[j]) {
-//         output[key] = source[values];
+//       if (entries[i][0] === keys[j]) {
+//         delete entries[i];
+//         output[entries[i][0]] = entries[i][1];
 //       }
 //     }
 //   }
+//   console.log(output);
 //   return output;
 // }
