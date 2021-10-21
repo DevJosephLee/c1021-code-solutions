37e07b755c0abcaf3a956abf642ac6f082d5ed7e
Joseph Lee <teddylou0215@gmail.com>
2021-10-21 15:35:12 -0700

    in progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,9 +1,17 @@
 /* exported isAnagram */
 function isAnagram(firstString, secondString) {
-  var noSpaceFirstString = firstString.split(' ').join('');
-  var noSpaceSecondString = secondString.split(' ').join('');
-  return noSpaceFirstString.includes(noSpaceSecondString);
-
+  var noSpaceFirstString = firstString.split(' ');
+  var noSpaceSecondString = secondString.split(' ');
+  for (var i = 0; i < noSpaceFirstString.length; i++) {
+    // console.log(noSpaceFirstString[i]);
+    for (var j = 0; j < noSpaceSecondString.length; j++) {
+      // console.log(noSpaceSecondString[j]);
+      if (noSpaceFirstString[i] === noSpaceSecondString[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 // for (var i = 0; i < noSpaceFirstString.length; i++) {
@@ -16,3 +24,12 @@ function isAnagram(firstString, secondString) {
 // }
 //   }
 // return true;
+
+// if (noSpaceFirstString.includes(noSpaceSecondString)) {
+//   return true;
+// } else if ()
+// if (firstString.includes(secondString) && firstString.length === secondString.length) {
+//   return true;
+// } else {
+//   return false;
+// }
