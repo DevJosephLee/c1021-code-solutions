987efcf66381b7128620b5a26f718e89a8544e35
Joseph Lee <teddylou0215@gmail.com>
2021-10-21 17:23:10 -0700

    Completed javascript-view-swapping exercise

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,23 @@
+var $tabContainer = document.querySelector('.tab-container');
+var $tabNodeList = document.querySelectorAll('.tab');
+var $viewNodeList = document.querySelectorAll('.view');
+
+$tabContainer.addEventListener('click', function () {
+  if (event.target.matches('.tab')) {
+    for (var i = 0; i < $tabNodeList.length; i++) {
+      if (event.target === $tabNodeList[i]) {
+        $tabNodeList[i].className = 'tab active';
+      } else {
+        $tabNodeList[i].className = 'tab';
+      }
+    }
+    var $dataViewEventTarget = event.target.getAttribute('data-view');
+    for (var j = 0; j < $viewNodeList.length; j++) {
+      if ($dataViewEventTarget === $viewNodeList[j].getAttribute('data-view')) {
+        $viewNodeList[j].className = 'view';
+      } else {
+        $viewNodeList[j].className = 'view hidden';
+      }
+    }
+  }
+});
