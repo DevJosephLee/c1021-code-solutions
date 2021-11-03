a2769c0bc85a2710c264025f6a7c7fc2c6e2fb2c
Joseph Lee <teddylou0215@gmail.com>
2021-11-03 10:02:01 -0700

    in progress

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,10 +1,21 @@
 /* exported equal */
+// if the length of the first list and second list are not the same
+//  give back false
+//  else if the first item on first list and first item on second list are not the same
+//  give back false
+// look at each item on the first list
+//  if the second list of items does not include the first list of items
+//    give back false
+//  give back grue
 function equal(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  } else if (first[0] !== second[0]) {
+    return false;
+  }
   for (var i = 0; i < first.length; i++) {
-    for (var j = 0; j < second.length; j++) {
-      if (first[i] !== second[j]) {
-        return false;
-      }
+    if (!second.includes(first[i])) {
+      return false;
     }
   }
   return true;
--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,5 @@
 /* exported flatten */
+function flatten(array) {
+  var output = [].concat.apply([], array);
+  return output;
+}
