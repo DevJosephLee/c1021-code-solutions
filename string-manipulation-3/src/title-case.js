85a2815d4988a35227d4464271688ee86933f030
Joseph Lee <teddylou0215@gmail.com>
2021-11-02 20:39:56 -0700

    in progress

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,18 +1,32 @@
 /* exported titleCase */
 function titleCase(title) {
-  // console.log(title[0].toUpperCase());
-  // var output = '';
-  // var currentWord = '';
-  // for (var i = 0; i < title.length; i++) {
-  // if (title[i] !== ' ') {
-  //   currentWord = title[0].toUpperCase() + title.slice(1).toLowerCase();
-  //   console.log(currentWord);
-  // }
-  // }
-  // output += currentWord;
-  // return output;
+  var upperCaseWordsInArray = [];
+  var titleSplit = title.split(' ');
+  for (var i = 0; i < titleSplit.length; i++) {
+    if (titleSplit[i].toLowerCase() === 'javascript') {
+      titleSplit[i] = 'JavaScript';
+    } else if (titleSplit[i].toLowerCase() === 'api') {
+      titleSplit[i] = 'API';
+    } else if (titleSplit[i].length < 4) {
+      upperCaseWordsInArray.push(titleSplit[i]);
+    } else {
+      upperCaseWordsInArray.push(titleSplit[i][0].toUpperCase() + titleSplit[i].slice(1).toLowerCase());
+    }
+  }
+  // return upperCaseWordsInArray.join(' ');
 }
 
+// console.log(title[0].toUpperCase());
+// var output = '';
+// var currentWord = '';
+// for (var i = 0; i < title.length; i++) {
+// if (title[i] !== ' ') {
+//   currentWord = title[0].toUpperCase() + title.slice(1).toLowerCase();
+//   console.log(currentWord);
+// }
+// }
+// output += currentWord;
+// return output;
 // else {
 //   output += currentWord + ' ';
 //   currentWord = '';
@@ -48,3 +62,27 @@ function titleCase(title) {
 // }
 // output += currentWord;
 // return output;
+
+// var output = '';
+// var currentWord = '';
+// for (var i = 0; i < title.length; i++) {
+//   if (title[i] !== ' ') {
+//     currentWord = title[0].toUpperCase() + title.slice(1).toLowerCase();
+//     console.log(currentWord);
+//   }
+// }
+
+// console.log(upperCaseWordsInArray);
+// var output = upperCaseWordsInArray.join(' ');
+// return output;
+// console.log(upperCaseWordsInArray.length);
+// for (var j = 0; j < upperCaseWordsInArray.length; j++) {
+//   if (upperCaseWordsInArray[j].length < 4) {
+//     upperCaseWordsInArray[j].toLowerCase();
+//   }
+// }
+// console.log(upperCaseWordsInArray);
+// var output = upperCaseWordsInArray.join(' ');
+// for (var j = 0; j < output.length; j++) {
+//   if ()
+// }
