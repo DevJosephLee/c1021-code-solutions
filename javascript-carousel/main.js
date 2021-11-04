9f45aa64fdc4f8d3d9fff9f192403e83a7cc85d3
Joseph Lee <teddylou0215@gmail.com>
2021-11-03 17:25:40 -0700

    Completed javascript-carousel exercise

---
+++ javascript-carousel/main.js
@@ -0,0 +1,95 @@
+var $buttonContainer = document.querySelector('.button-container');
+var $buttonNodeList = document.querySelectorAll('button');
+var $imgNodeList = document.querySelectorAll('img');
+var $leftArrow = document.querySelector('.fa-chevron-left');
+var $rightArrow = document.querySelector('.fa-chevron-right');
+var currentDataNumber = 1;
+
+setInterval(switchImgs, 2000);
+
+function switchImgs() {
+  currentDataNumber++;
+  changeButtonColor();
+  for (var i = 0; i < $imgNodeList.length; i++) {
+    if (currentDataNumber === parseInt($imgNodeList[i].getAttribute('dataid'))) {
+      $imgNodeList[i].className = 'view';
+    } else {
+      $imgNodeList[i].className = 'hidden';
+    }
+  }
+  if (currentDataNumber === 5) {
+    currentDataNumber = 0;
+  }
+}
+
+function clickButtons(event) {
+  if (!event.target.matches('button')) {
+    return;
+  }
+  for (var j = 0; j < $buttonNodeList.length; j++) {
+    if (event.target === $buttonNodeList[j]) {
+      $buttonNodeList[j].className = 'black';
+    } else {
+      $buttonNodeList[j].className = 'white';
+    }
+  }
+  var $dataViewEventTarget = event.target.getAttribute('data-view');
+  for (var i = 0; i < $imgNodeList.length; i++) {
+    if ($imgNodeList[i].getAttribute('data-view') === $dataViewEventTarget) {
+      $imgNodeList[i].className = 'view';
+    } else {
+      $imgNodeList[i].className = 'hidden';
+    }
+  }
+  currentDataNumber = event.target.getAttribute('dataid');
+  var interval = setInterval(switchImgs, 2000);
+  clearInterval(interval);
+}
+
+function rightArrowClick(event) {
+  currentDataNumber++;
+  changeButtonColor();
+  for (var i = 0; i < $imgNodeList.length; i++) {
+    if (currentDataNumber === parseInt($imgNodeList[i].getAttribute('dataid'))) {
+      $imgNodeList[i].className = 'view';
+    } else {
+      $imgNodeList[i].className = 'hidden';
+    }
+  }
+  if (currentDataNumber === 5) {
+    currentDataNumber = 0;
+  }
+  var interval = setInterval(switchImgs, 2000);
+  clearInterval(interval);
+}
+
+function leftArrowClick(event) {
+  currentDataNumber--;
+  if (currentDataNumber === 0) {
+    currentDataNumber = 5;
+  }
+  changeButtonColor();
+  for (var i = 0; i < $imgNodeList.length; i++) {
+    if (currentDataNumber === parseInt($imgNodeList[i].getAttribute('dataid'))) {
+      $imgNodeList[i].className = 'view';
+    } else {
+      $imgNodeList[i].className = 'hidden';
+    }
+  }
+  var interval = setInterval(switchImgs, 2000);
+  clearInterval(interval);
+}
+
+function changeButtonColor() {
+  for (var j = 0; j < $buttonNodeList.length; j++) {
+    if (currentDataNumber === parseInt($buttonNodeList[j].getAttribute('dataid'))) {
+      $buttonNodeList[j].className = 'black';
+    } else {
+      $buttonNodeList[j].className = 'white';
+    }
+  }
+}
+
+$buttonContainer.addEventListener('click', clickButtons);
+$rightArrow.addEventListener('click', rightArrowClick);
+$leftArrow.addEventListener('click', leftArrowClick);
