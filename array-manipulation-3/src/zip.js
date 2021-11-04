92353a7902c8ae829043e6364eea1429130de587
Joseph Lee <teddylou0215@gmail.com>
2021-11-04 09:56:59 -0700

    in progress

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,8 +1,17 @@
 /* exported zip */
-// function zip(first, second) {
-//   var output = [];
-//   var temp = [];
-//   for (var i = 0; i < first.length; i++) {
-
-//   }
-// }
+function zip(first, second) {
+  // var firstCopy = [];
+  // var secondCopy = [];
+  // firstCopy.push(first);
+  // secondCopy.push(second);
+  // for (var i = 0; i < first.length; i++) {
+  //   for (var j = 0; j < second.length; j++) {
+  //     if (firstCopy[i].length !== secondCopy[j].length) {
+  //       firstCopy[i].pop();
+  //       secondCopy[j].pop();
+  //       console.log('first', firstCopy[i]);
+  //       console.log('second', secondCopy[j]);
+  //     }
+  //   }
+  // }
+}
