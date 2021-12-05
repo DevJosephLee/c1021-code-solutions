0c6363410dfe60c2b376f31df96a275abc9afb64
Joseph Lee <teddylou0215@gmail.com>
2021-12-04 20:32:24 -0800

    Completed sql-aggregates exercise

--- sql-aggregates/movie-maniacs.sql
+++ sql-aggregates/movie-maniacs.sql
@@ -1,11 +1,6 @@
--- select "firstName",
---        "lastName",
---        sum("payments"."amount") as "totalAmountSpent"
--- from   "customers"
--- join   "payments" using ("amount")
--- group by "customers"."firstName"
---      and "customers"."lastName"
-
-
-select sum("amount") as "totalAmountSpent"
-from   "payments"
+select concat("firstName", ' ', "lastName") as "name",
+       sum("payments"."amount") as "totalAmountSpent"
+from   "customers"
+join   "payments" using ("customerId")
+group by concat("customers"."firstName", ' ', "customers"."lastName")
+order by "totalAmountSpent" desc
