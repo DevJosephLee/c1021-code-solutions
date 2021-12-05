c263c18bb601fd8f51dbddc7a596a30813563b62
Joseph Lee <teddylou0215@gmail.com>
2021-12-05 11:02:45 +0000

    fix issues

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,47 +1,16 @@
 /* exported zip */
 
 // make a storage for output (empty array)
-// make a storage for temp (empty array)
 // make a storage for the shorter length of first and second
 // look at each item from the first array, starting from beginning until the shorter length between first and second
-//    assign items from first array to temp
-//    assign same amount of items as first array to second array to temp
-// look at each item from temp array, starting from beginning until end, in chuncks of two
-//    assign items from temp, in chuncks of two, to the output array
+//    assign first two elements of each array into output
 // give back output
 
-// function zip(first, second) {
-//   var output = [];
-//   var temp = [];
-//   var length = Math.min(first.length, second.length);
-//   for (var i = 0; i < length; i++) {
-//     temp.push(first[i]);
-//     temp.push(second[i]);
-//   }
-//   for (var j = 0; j < temp.length; j += 2) {
-//     output.push(temp.slice(j, j + 2));
-//   }
-//   return output;
-// }
-
-// function zip(first, second) {
-//   var output = [];
-//   var length = Math.min(first.length, second.length);
-//   for (var i = 0; i < length; i += 2) {
-//     console.log(first.slice(i, i + 2));
-//     console.log(second.slice(i, i + 2));
-//   }
-// }
-
-// function zip(first, second) {
-//   var output = [];
-//   var temp = [];
-//   var index = 0;
-//   var shorterLength = Math.min(first.length, second.length);
-//   while (index !== shorterLength) {
-//     temp.push(first[index], second[index]);
-//     output.push(temp);
-//     index++;
-//   }
-//   console.log(output);
-// }
+function zip(first, second) {
+  var output = [];
+  var length = Math.min(first.length, second.length);
+  for (var i = 0; i < length; i++) {
+    output.push([].concat(first[i], second[i]));
+  }
+  return output;
+}
