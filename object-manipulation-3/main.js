ffbd5aa94d55bc378659d43ca455ca525e82fc8c
Joseph Lee <teddylou0215@gmail.com>
2021-12-05 10:29:43 +0000

    fix issues

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -49,80 +49,42 @@ players[3].hand[1] = shuffledDeck[7];
 function calculateScore() {
   for (var i = 0; i < players.length; i++) {
     players[i].score = players[i].hand[0].rank + players[i].hand[1].rank;
-    // if (players[i].score.length === 3) {
-    //   return;
-    // }
     if (typeof players[i].score === 'string') {
-      var notStrings = players[i].score;
-    }
-    for (var j = 0; j < notStrings.length; j++) {
-      // if (playersScoreSplit[j] === 'J') {
-      //   playersScoreSplit[j] = 10;
-      // } else if (playersScoreSplit[j] === 'Q') {
-      //   playersScoreSplit[j] = 10;
-      // } else if (playersScoreSplit[j] === 'K') {
-      //   playersScoreSplit[j] = 10;
-      // } else if (playersScoreSplit[j] === 'A') {
-      //   playersScoreSplit[j] = 11;
-      // }
-      console.log(notStrings[j]);
+      var playerScoreSplit = players[i].score.split('');
+      for (var j = 0; j < playerScoreSplit.length; j++) {
+        if (playerScoreSplit[j] === 'J') {
+          playerScoreSplit[j] = 10;
+        } else if (playerScoreSplit[j] === 'Q') {
+          playerScoreSplit[j] = 10;
+        } else if (playerScoreSplit[j] === 'K') {
+          playerScoreSplit[j] = 10;
+        } else if (playerScoreSplit[j] === 'A') {
+          playerScoreSplit[j] = 11;
+        } else if (typeof playerScoreSplit[j] !== 'number') {
+          playerScoreSplit[j] = parseInt(playerScoreSplit[j]);
+        }
+      }
+      if (playerScoreSplit[0] === 1) {
+        playerScoreSplit = [10, playerScoreSplit[2]];
+      } else if (playerScoreSplit[1] === 1) {
+        playerScoreSplit = [playerScoreSplit[0], 10];
+      }
+      players[i].score = playerScoreSplit[0] + playerScoreSplit[1];
+    } else {
+      players[i].score = Number(players[i].score);
     }
   }
 }
 
 calculateScore();
 
-// for (var j = 0; j < players[i].hand.length; j++) {
+function decideWinner() {
+  var scores = [];
+  for (var i = 0; i < players.length; i++) {
+    scores.push(players[i].score);
+  }
+  var indexOfWinner = scores.indexOf(Math.max.apply(null, scores));
+  console.log('The winner is ' + players[indexOfWinner].name + ' with a score of ' + Math.max.apply(null, scores));
+}
 
-// }
-
-// if (players[i].hand[j].rank === 'J') {
-//   players[i].score += 10;
-// } else if (players[i].hand[j].rank === 'Q') {
-//   players[i].score += 10;
-// } else if (players[i].hand[j].rank === 'K') {
-//   players[i].score += 10;
-// }
-
-// function decideWinner() {
-//   var scores = [];
-//   for (var i = 0; i < players.length; i++) {
-//     scores.push(players[i].score);
-//   }
-//   var indexOfWinner = scores.indexOf(Math.max.apply(null, scores));
-//   console.log('The winner is ' + players[indexOfWinner].name + ' with a score of ' + Math.max.apply(null, scores));
-// }
-
-// decideWinner();
-
-// if (players[i].hand1.rank === 'J' || players[i].hand2.rank === 'J') {
-//   players[i].score += 10;
-// } else if (players[i].hand1.rank === 'Q' || players[i].hand2.rank === 'Q') {
-//   players[i].score += 10;
-// } else if (players[i].hand1.rank === 'K' || players[i].hand2.rank === 'K') {
-//   players[i].score += 10;
-// } else if (players[i].hand1.rank === 'A' || players[i].hand2.rank === 'A') {
-//   players[i].score += 11;
-// } else {
-//   players[i].score = players[i].hand1.rank + players[i].hand2.rank;
-// }
-
-// function calculateScore() {
-//   for (var i = 0; i < deck.length; i++) {
-//     if (deck[i].rank === 'jack') {
-//       deck[i].rank = 10;
-//     } else if (deck[i].rank === 'queen') {
-//       deck[i].rank = 10;
-//     } else if (deck[i].rank === 'king') {
-//       deck[i].rank = 10;
-//     } else if (deck[i].rank === 'ace') {
-//       deck[i].rank = 11;
-//     }
-//   }
-//   players[0].score = players[0].hand1.rank + players[0].hand2.rank;
-//   players[1].score = players[1].hand1.rank + players[1].hand2.rank;
-//   players[2].score = players[2].hand1.rank + players[2].hand2.rank;
-//   players[3].score = players[3].hand1.rank + players[3].hand2.rank;
-// }
-
-// calculateScore();
+decideWinner();
