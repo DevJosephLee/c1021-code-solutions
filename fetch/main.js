8c5abcbc7552864714c3945c69e5853426e2fece
Joseph Lee <teddylou0215@gmail.com>
2021-12-15 12:32:20 -0800

    minor fix

--- fetch/main.js
+++ fetch/main.js
@@ -5,7 +5,7 @@ fetch('https://jsonplaceholder.typicode.com/users', {
   .then(data => console.log(data));
 
 fetch('https://pokeapi.co/api/v2/pokemon/6', {
-  method: 'Get'
+  method: 'GET'
 })
   .then(response => response.json())
   .then(data => console.log(data));
