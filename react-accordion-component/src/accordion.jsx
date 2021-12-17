21451326fe8355feb9ba24a00a3cebb7c22a7c60
Joseph Lee <teddylou0215@gmail.com>
2021-12-17 18:30:50 +0000

    fix issues

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-const topics = [
-  { title: 'Hypertext Markup Language', description: 'The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.' },
-  { title: 'Cascading Style Sheets', description: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.' },
-  { title: 'JavaScript', description: "JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. Over 97% of websites use Javascript on the client side for web page behavior,often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on the user's device." }
-]
-
 class Accordion extends React.Component {
   constructor(props) {
     super(props);
@@ -13,19 +7,14 @@ class Accordion extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick(event) {
+    this.setState({ openTopicIndex: Number(event.target.className) });
     if (this.state.openTopicIndex !== null && Number(event.target.className) === Number(this.state.openTopicIndex)) {
       this.setState({ openTopicIndex: null });
-    } else if (event.target.className === '0') {
-      this.setState({ openTopicIndex: 0 });
-    } else if (event.target.className === '1') {
-      this.setState({ openTopicIndex: 1 });
-    } else if (event.target.className === '2') {
-      this.setState({ openTopicIndex: 2 });
     }
   }
   render() {
     return (
-      topics.map((topic, index) => {
+      this.props.topics.map((topic, index) => {
         return (
           <div key={index}>
             <button className={index} onClick={this.handleClick}>{topic.title}</button>
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './accordion';
 
+const topics = [
+  { title: 'Hypertext Markup Language', description: 'The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.' },
+  { title: 'Cascading Style Sheets', description: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.' },
+  { title: 'JavaScript', description: "JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. Over 97% of websites use Javascript on the client side for web page behavior,often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on the user's device." }
+]
+
 ReactDOM.render(
-  <Accordion />,
+  <Accordion topics={topics} />,
   document.querySelector('#root')
 )
