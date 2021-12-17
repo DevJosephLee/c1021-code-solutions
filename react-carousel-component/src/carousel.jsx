5d5c1f03d437310ea67641633073fba70b40b478
Joseph Lee <teddylou0215@gmail.com>
2021-12-17 00:57:53 +0000

    in progress

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+const pokemons =[
+  {name: 'Eevee', url: 'https://archives.bulbagarden.net/media/upload/e/e2/133Eevee.png'},
+  {name: 'Abomasnow', url: 'https://archives.bulbagarden.net/media/upload/3/3b/460Abomasnow.png'}
+]
+
 class Carousel extends React.Component {
   constructor(props) {
     super(props)
@@ -11,12 +16,17 @@ class Carousel extends React.Component {
   }
   render() {
     return(
-      <div className="container">
-        <div className="row justify-center align-center">
-          <i class="fas fa-chevron-left"></i>
-          <i class="fas fa-chevron-right"></i>
-        </div>
-      </div>
+      pokemons.map((pokemon) => {
+        return(
+          <div className="container">
+            <div className="row justify-center padding-auto">
+              <i class="fas fa-chevron-left"></i>
+              <img key={pokemon.name} src={pokemon.url} / >
+              <i class="fas fa-chevron-right"></i>
+            </div>
+          </div>
+        )
+      })
     )
   }
 }
