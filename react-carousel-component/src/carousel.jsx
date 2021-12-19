562a8652998bffe76b1b03f314f583dbc3a6251c
Joseph Lee <teddylou0215@gmail.com>
2021-12-19 10:03:13 +0000

    Completed react-carousel-component exercise

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,32 +1,60 @@
 import React from 'react';
 
-const pokemons =[
-  {name: 'Eevee', url: 'https://archives.bulbagarden.net/media/upload/e/e2/133Eevee.png'},
-  {name: 'Abomasnow', url: 'https://archives.bulbagarden.net/media/upload/3/3b/460Abomasnow.png'}
-]
+let intervalId;
 
 class Carousel extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
-    this.handleClick = this.handleClick.bind(this);
+    this.state = {currentImageIndex: 0}
+    this.handleLeftClick = this.handleLeftClick.bind(this);
+    this.handleRightClick = this.handleRightClick.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
   }
-  handleClick() {
-
+  componentDidMount() {
+    intervalId = setInterval(() => {
+      if (this.state.currentImageIndex === this.props.pokemons.length - 1) {
+        this.setState({ currentImageIndex: -1 })
+      }
+      this.setState({ currentImageIndex: this.state.currentImageIndex + 1 });
+    }, 2000)
+  }
+  handleLeftClick() {
+    this.setState({currentImageIndex: this.state.currentImageIndex - 1});
+    if (this.state.currentImageIndex === 0) {
+      this.setState({ currentImageIndex: this.props.pokemons.length - 1});
+    }
+  }
+  handleRightClick() {
+    this.setState({currentImageIndex: this.state.currentImageIndex + 1});
+    if (this.state.currentImageIndex === this.props.pokemons.length - 1) {
+      this.setState({ currentImageIndex: 0});
+    }
+  }
+  handleButtonClick(event) {
+    this.setState({ currentImageIndex: Number(event.target.className[0])})
   }
   render() {
-    return(
-      pokemons.map((pokemon) => {
-        return(
-          <div className="container">
-            <div className="row justify-center padding-auto">
-              <i class="fas fa-chevron-left"></i>
-              <img key={pokemon.name} src={pokemon.url} / >
-              <i class="fas fa-chevron-right"></i>
-            </div>
+    return (
+      <div>
+        <div className="container">
+          <div className="center">
+            <i className="fas fa-chevron-left" onClick={this.handleLeftClick}></i>
+            {this.props.pokemons.map((pokemon, index) => {
+              return (
+                <img className={this.state.currentImageIndex === index ? 'show' : 'hidden'} key={pokemon.name} src={pokemon.url} />
+              )
+            })}
+            <i className="fas fa-chevron-right" onClick={this.handleRightClick}></i>
           </div>
-        )
-      })
+          <div>
+            {this.props.pokemons.map((pokemon, index) => {
+              return (
+                <button onClick={this.handleButtonClick} className={`${index} ${this.state.currentImageIndex === index ? 'black' : 'white'}`} key={pokemon.name} ></button>
+              )
+            })}
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Carousel from './carousel';
 
+const pokemons = [
+  { name: 'Rayquaza', url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/384.png'},
+  { name: 'Charizard', url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png'},
+  { name: 'Zapdos', url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/145.png'},
+  { name: 'Entei', url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/244.png'},
+  { name: 'Mew', url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png'}
+]
+
 ReactDOM.render(
-  <Carousel />,
+  <Carousel pokemons={pokemons} />,
   document.querySelector('#root')
 )
