5222ad0424864146a8469a359f13ca1f6de0dc25
Joseph Lee <teddylou0215@gmail.com>
2022-01-11 00:26:35 +0000

    fix big-o-notation

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -25,19 +25,19 @@ function uniqueQuadratic(words) {
     i++                             // 2 * n = O(2n)
   ) {
     const word = words[i];          // 2 * n = O(2n)
-    let isUnique = true;            // 1 * 1 = O(1)
+    let isUnique = true;            // 1 * n = O(n)
     for (
-      let c = 0;                    // 1 * 1 = O(1)
-      c < i;                        // n * n = O(n^2)
-      c++                           // 2 * n = O(2n)
+      let c = 0;                    // 1 * n = O(n)
+      c < i;                        // 1 * n^2 = O(n^2)
+      c++                           // 2 * n^2 = O(2n^2)
     ) {
-      const comparing = words[c];   // 2 * n = O(2n)
-      if (comparing === word) {     // n * n = O(n^2)
-        isUnique = false;           // 1 * 1 = O(1)
+      const comparing = words[c];   // 2 * n^2 = O(2n^2)
+      if (comparing === word) {     // 1 * n^2 = O(n^2)
+        isUnique = false;           // 1 * n^2 = O(n^2)
       }
     }
-    if (isUnique) {                 // 1 * 1 = O(1)
-      unique[unique.length] = word; // 3 * n = O(n)
+    if (isUnique) {                 // 1 * n = O(n)
+      unique[unique.length] = word; // 3 * n = O(3n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
